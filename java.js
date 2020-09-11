bf0b2e3b3fe7201ba2abedd39ee8ba56dc8203af
AsadAyaz22 <71154701+AsadAyaz22@users.noreply.github.com>
2020-09-12 02:58:48 +0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,55 @@
+function change()
+{
+    document.getElementById("demo").innerHTML="Empty"
+    document.getElementById("demo1").innerHTML="Empty"
+    document.getElementById("demo2").innerHTML="Empty"
+    document.getElementById("demo3").innerHTML="Empty"
+    document.getElementById("demo4").innerHTML="Empty"
+    document.getElementById("demo5").innerHTML="Empty"
+    document.getElementById("demo6").innerHTML="Empty"
+}
+function change1()
+{
+    document.getElementById("demo7").innerHTML="New Todo List"
+    document.getElementById("demo").innerHTML="Empty"
+    document.getElementById("demo1").innerHTML="Empty"
+    document.getElementById("demo2").innerHTML="Empty"
+    document.getElementById("demo3").innerHTML="Empty"
+    document.getElementById("demo4").innerHTML="Empty"
+    document.getElementById("demo5").innerHTML="Empty"
+    document.getElementById("demo6").innerHTML="Empty"
+    document.getElementById("demo16").innerHTML="Please Enter Your Task"
+}
+
+function change2()
+{ 
+    document.getElementById("demo8").innerHTML="Done"
+}
+    function change3()
+{
+    document.getElementById("demo9").innerHTML="Done"
+}
+function change4()
+{
+    document.getElementById("demo10").innerHTML="Done"
+}
+function change5()
+{    
+    document.getElementById("demo11").innerHTML="Done"
+}
+function change6()
+{
+    document.getElementById("demo12").innerHTML="Done"
+}
+function change7()
+{
+    document.getElementById("demo13").innerHTML="Done"
+}
+function change8()
+{
+document.getElementById("demo14").innerHTML="Done"
+}
+function change9()
+{
+document.getElementById("demo15").innerHTML="Please Enter Your Task"
+}
